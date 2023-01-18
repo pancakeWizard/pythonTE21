99d8719445086b68553778a9d8c94ecfe226d218
pancakeWizard <114063417+pancakeWizard@users.noreply.github.com>
2023-01-18 14:30:26 +0100

    LessonTwo

---
+++ Data types and Operators/lessonTwo.py
@@ -0,0 +1,47 @@
+#region_ = "North"
+# firstName = "Tony"
+
+# sale = 30000
+# serialNumber = "53235"
+
+# itemReceived = False
+
+# itemOrdered = ["Snowblower", "Skis", "Chapstick"]
+
+# sales = 32000
+# print(sales)
+
+
+
+# a = 15
+# b = 25
+# c = 15
+# print(a == b)
+# print(a == c)
+# print(a > c)
+# print(a >= c)
+# print(a <= b)
+# print(a != c)
+
+
+
+
+
+# a = 3
+# b = a
+
+# northItems = ["Rain gear", "Snow shoes"]
+# eastItems = ["Rain gear", "Snow shoes"]
+
+# print (a == b)
+# print (a is b)
+
+# print(northItems == eastItems)
+# print(northItems is eastItems)
+
+name = "Python"
+secondName = "3.0"
+phone = "0700000000"
+address = "Your computer, C:/Users/UserName/AppData/Local/Programs/Python/Python311/python.exe"
+
+print("Name: " + name + " "+ secondName + "\nPhone nubmer: " + phone + "\nAddress: " + address)
