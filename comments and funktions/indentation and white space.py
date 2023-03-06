4703cf33e8954aa143d3ce0dc857367951154444
pancakeWizard <114063417+pancakeWizard@users.noreply.github.com>
2023-03-06 10:32:05 +0100

    indentation&white space

---
+++ comments and funktions/indentation and white space.py
@@ -0,0 +1,13 @@
+region = "West"
+annualSales = 500000
+firstName = "Tony"
+if annualSales >= 500000:
+    print("Gold customer")
+elif annualSales >= 30000:
+    print("Silver customer")
+elif annualSales >= 10000:
+    print("Bronze customer")
+print("Thank you for your business")
+print(f"Your sales representative is {firstName}," \ 
+      "you are in the {region} region, and you had" \ 
+      "{annualSales} in last year. Thanks!")
