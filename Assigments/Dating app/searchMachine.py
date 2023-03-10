1c2f53726d79d4a6566208617aa4fb59efec5a6b
pancakeWizard <114063417+pancakeWizard@users.noreply.github.com>
2023-03-10 11:33:26 +0100

    genderSearch is done?

--- Assigments/Dating app/searchMachine.py
+++ Assigments/Dating app/searchMachine.py
@@ -3,45 +3,49 @@ def cleanUserData(array,newArray):
         newArray[i] = newArray[i].replace(' ','')
         newArray[i] = newArray[i].replace('\n','')
         newArray[i] = newArray[i].lower()
+        if newArray[i] == "male":
+            newArray[i] = 1
+        elif newArray[i] == "female":
+            newArray[i] = 2
+        else:
+            newArray[i] = 3
 
 
 
 genderSearch = int(input("""Who would you like to date?
 1. Male
 2. Female
-3. Other"""))
+3. Other
+"""))
 def searchMachine(currentUser, genderSearch):
-
-
     with open ("Assigments/Dating app/tables/sex.txt", "r") as sex:
         sexes = sex.readlines()
         cleanUserData(sexes,sexes)
         print(sexes)
+
+    with open ("Assigments/Dating app/tables/preferences.txt","r") as preferences:
+        pref = preferences.readlines()
+        cleanUserData(pref,pref)
+
     sexFirstMatch = []
     sexDoubleMatch = []
     sexMatch = []
-    if genderSearch == 1:
-        for i in range(0, len(sexes)):
-            if sexes[i] == "male":
-                sexFirstMatch.append(i)
-                if currentUser in sexFirstMatch:
-                    sexFirstMatch.remove(currentUser)
-
-        with open ("Assigments/Dating app/tables/preferences.txt","r") as preferences:
-            pref = preferences.readlines()
-            cleanUserData(pref,pref)
-        for i in range(0, len(pref)):
-            if pref[i] == sexes[currentUser]:
-                sexDoubleMatch.append(i)
-    
-        for f in range(0, len(sexFirstMatch)):
-            for s in range(0, len(sexDoubleMatch)):
-                if sexFirstMatch[f] == sexDoubleMatch[s]:
-                    sexMatch.append(sexFirstMatch[f])
+    for i in range(0, len(sexes)):
+        if sexes[i] == genderSearch:
+            sexFirstMatch.append(i)
+            if currentUser in sexFirstMatch:
+                sexFirstMatch.remove(currentUser)
+    for l in range(0, len(pref)):
+        if pref[l] == sexes[currentUser]:
+            sexDoubleMatch.append(l)
+    for f in range(0, len(sexFirstMatch)):
+        for s in range(0, len(sexDoubleMatch)):
+            if sexFirstMatch[f] == sexDoubleMatch[s]:
+                sexMatch.append(sexFirstMatch[f])
     print(sexFirstMatch)
     print(sexes[currentUser])
     print(sexDoubleMatch)
     print(sexMatch)
 
-searchMachine(0, genderSearch)
+searchMachine(7, genderSearch)
 
